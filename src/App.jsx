5b5a2b742affb8a11d6f3f5db5c6a9d63059af75
Swapnil Ghodse <sgaicommunity@gmail.com>
2024-04-05 09:31:59 +0530

    Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,69 @@
-import Canvas from './components/Canvas'
-import { useDispatch, useSelector } from 'react-redux'
-import { changeStrokeSize, clearScreen, pickColor } from './redux/drawSplice';
+import Canvas from "./components/Canvas";
+import { useDispatch, useSelector } from "react-redux";
+import { changeStrokeSize, clearScreen, pickColor } from "./redux/drawSplice";
 
 function App() {
-
   const dispatch = useDispatch();
-  const strokeColor = useSelector( (state)=> state.draw.strokeColor );
-  const strokeSize = useSelector( state => state.draw.strokeSize);
+  const strokeColor = useSelector((state) => state.draw.strokeColor);
+  const strokeSize = useSelector((state) => state.draw.strokeSize);
 
-  const clearScreenFunction = ()=>{
-      const canvas = document.querySelector("#signature-canvas");
-      const ctx = canvas.getContext("2d");
-      ctx.clearRect(0, 0, 500, 500);
+  const clearScreenFunction = () => {
+    const canvas = document.querySelector("#signature-canvas");
+    const ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, window.screen.width, window.screen.height);
+  };
+
+  const downloadImage = () => {
+      let link = document.createElement('a');
+      link.download = 'signature.png';
+      link.href = document.getElementById('signature-canvas').toDataURL()
+      link.click();
   }
-  
-
-
 
   return (
-    <>
-      <div >
-        <button className=" btn btn-outline btn-error" onClick={()=>clearScreenFunction() }>Clear</button>
-        <input type='color' id='drawColorPicker' onChange={(event) =>{dispatch(pickColor(event.target.value))}} value={strokeColor} />
-        <input type='range' id='strokeSize' value={strokeSize} min={1} max={50} onChange={(event) =>{dispatch(changeStrokeSize(event.target.value))}} />  
+    <div className=" container mx-auto pt-1 flex flex-col">
+      <div className="py-7">
+        <div className="flex justify-around items-center pb-8">
+          <button
+            className=" btn btn-error"
+            onClick={() => clearScreenFunction()}
+          >
+            Clear
+          </button>
+          <input
+            type="color"
+            id="drawColorPicker"
+            onChange={(event) => {
+              dispatch(pickColor(event.target.value));
+            }}
+            value={strokeColor}
+            className=" w-32 h-12 rounded-md"
+          />
+
+          <span className="flex items-center ">
+            <input
+              type="range"
+              id="strokeSize"
+              value={strokeSize}
+              min={1}
+              max={50}
+              className=""
+              onChange={(event) => {
+                dispatch(changeStrokeSize(event.target.value));
+              }}
+            />
+            <p className="m-4 w-2">{strokeSize}</p>
+          </span>
+        </div>
       </div>
-      <Canvas />
-      </>
-  )
+      <div >
+        <Canvas />
+      </div>
+      <div className="m-auto py-4">
+        <button type="button" className="btn btn-success base-100" onClick={()=>downloadImage()}>Download</button>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -2,11 +2,10 @@ import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 
 function Canvas() {
-  const strokeColor = useSelector( (state)=> state.draw.strokeColor );
-  const strokeSize = useSelector( (state)=> state.draw.strokeSize );
-  
-  useEffect(() => {
+  const strokeColor = useSelector((state) => state.draw.strokeColor);
+  const strokeSize = useSelector((state) => state.draw.strokeSize);
 
+  useEffect(() => {
     const canvas = document.querySelector("#signature-canvas");
 
     const ctx = canvas.getContext("2d");
@@ -14,50 +13,43 @@ function Canvas() {
     ctx.strokeStyle = strokeColor;
     ctx.lineWidth = strokeSize;
 
-    ctx.lineJoin = 'round';
-    ctx.lineCap = 'round';
-    
-    
+    ctx.lineJoin = "round";
+    ctx.lineCap = "round";
+
     let isDrawing = false;
 
     let lastX = 0;
     let lastY = 0;
 
-    const draw = (event) =>{
-        if(!isDrawing) return;
+    const draw = (event) => {
+      if (!isDrawing) return;
 
-        ctx.beginPath();
+      ctx.beginPath();
 
-        ctx.moveTo(lastX, lastY);
-        ctx.lineTo(event.offsetX, event.offsetY);
-        ctx.stroke();
+      ctx.moveTo(lastX, lastY);
+      ctx.lineTo(event.offsetX, event.offsetY);
+      ctx.stroke();
 
-        
-        [lastX, lastY] = [event.offsetX, event.offsetY];
+      [lastX, lastY] = [event.offsetX, event.offsetY];
+    };
 
-    }
-
-    canvas.addEventListener('mousemove', draw);
-    canvas.addEventListener('mousedown', (event)=>{
-        isDrawing = true;
-        [lastX, lastY] = [event.offsetX, event.offsetY];
+    canvas.addEventListener("mousemove", draw);
+    canvas.addEventListener("mousedown", (event) => {
+      isDrawing = true;
+      [lastX, lastY] = [event.offsetX, event.offsetY];
     });
-    
-  canvas.addEventListener('mouseup', ()=>isDrawing=false);
-  canvas.addEventListener('mouseout', ()=>isDrawing=false);
 
-
+    canvas.addEventListener("mouseup", () => (isDrawing = false));
+    canvas.addEventListener("mouseout", () => (isDrawing = false));
   }, [strokeColor, strokeSize]);
 
   return (
-    <div>
-      <canvas
-        id="signature-canvas"
-        width="500"
-        height="500"
-        className=" border-solid border-2 border-black"
-      ></canvas>
-    </div>
+    <canvas
+      id="signature-canvas"
+      width={1200}
+      height={700}
+      className=" border-solid border-2 border-black px-0 mx-auto block w-[1200px]"
+    ></canvas>
   );
 }
 
